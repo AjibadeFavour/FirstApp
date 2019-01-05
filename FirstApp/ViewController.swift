698a132f87db27bc82365b0bf354c9bab4f2a98c
Favour <Favour@me865s-MacBook-Pro.local>
2019-01-05 12:21:59 +0200

    This is the first commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Favour on 05/01/2019.
+//  Copyright © 2019 Favour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
